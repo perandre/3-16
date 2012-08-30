59eeda285f25b6cf3557b90c3dd5cf35fc467699
Thor Andre Gretland <post@thorandre.net>
2012-08-30 23:34:59 +0200

    Changes for make front page slider work with Norwegian content and 3-16 content types

--- sites/all/themes/designmd/views/views-view-fields--front-slider-block--block.tpl.php
+++ sites/all/themes/designmd/views/views-view-fields--front-slider-block--block.tpl.php
@@ -3,7 +3,7 @@
 image_style_url('1200x570', $row->_field_data['nid']['entity']->field_image['nb'][0]['uri']); ?>" alt="Bilder" />
  <div>
 <h2><?php print $fields['title']->content; ?></h2>
-<?php print $fields['field_	body']->content; ?>
+<?php print $fields['field_body']->content; ?>
 </div>
 </li>
  
